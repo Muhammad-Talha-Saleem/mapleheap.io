484cfcac96d0b2f5dc1ad19532ee7a7eef4a69af
Talha Hori <talha.solutionq786@gmail.com>
2024-05-05 17:57:35 +0500

    UI Changes

--- js/footer.js
+++ js/footer.js
@@ -15,10 +15,24 @@ const footerContent = `
 	        </ul>
 	      </div>
 	      <div class="col-lg-3 col-sm-12">
-	        <b>About Us</b>
-	        <p class="mt-4 text-black-50">
-	          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo amet posuere porta vitae mi accumsan. Ultricies 
-	        </p>
+	        <b>Social Us</b>
+	        <ul class="list-unstyled mt-4 text-black-50">
+	          <li>
+	          	<a href="https://www.facebook.com/" title="Facebook Page" class="align-items-center d-flex text-dark text-decoration-none">
+	          		<i class="fa-brands fa-facebook-square pr-2 fa-2x"></i> Facebook Page
+	          	</a>
+	          </li>
+	          <li>
+	          	<a href="https://twitter.com/" title="Twitter" class="align-items-center d-flex text-dark text-decoration-none">
+	          		<i class="fa-brands fa-twitter-square pr-2 fa-2x"></i> Twitter
+	          	</a>
+	          </li>
+	          <li>
+	            <a href="https://www.instagram.com/" title="Instagram Page" class="align-items-center d-flex text-dark text-decoration-none">
+	            	<i class="fa-brands fa-instagram pr-2 fa-2x"></i> Instagram Page
+	            </a>
+	          </li>
+	        </ul>
 	      </div>
 	      <div class="col-lg-3 col-sm-12">
 	        <b>Subscribe</b>
@@ -26,12 +40,13 @@ const footerContent = `
 	          <input type="email" class="border-right-0 border-secondary form-control rounded-0" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
 	          <button class="border border-left-0 border-secondary btn rounded-0"><i class="fa fa-arrow-right"></i></button>
 	        </div>
-	        <b>Follow Us</b><br>
+	        <!-- <b>Follow Us</b><br>
 	        <span><a href="https://www.facebook.com/" title="facebook" class="text-decoration-none text-dark"><i class="fa-brands fa-facebook-square"></i></a></span>
 	        <span><a href="https://twitter.com/" title="facebook" class="text-decoration-none text-dark"><i class="fa-brands fa-twitter-square"></i></a></span>
-	        <span><a href="https://www.instagram.com/" title="facebook" class="text-decoration-none text-dark"><i class="fa-brands fa-instagram"></i></a></span>
+	        <span><a href="https://www.instagram.com/" title="facebook" class="text-decoration-none text-dark"><i class="fa-brands fa-instagram"></i></a></span> -->
 	      </div>
 	    </div>
 	  </div>
+	  <p class="m-0 p-0 text-center">Copyright © 2024 MapleHeap Technologies Inc. All rights reserved.</p>
 `;
 document.getElementById('_footer').innerHTML = footerContent;
